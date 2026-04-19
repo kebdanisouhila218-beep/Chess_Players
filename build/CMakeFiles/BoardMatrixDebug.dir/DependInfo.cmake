e694b7aeb0410fb81126816a15884d7ca32240a9
kebdanisouhila218-beep <kebdanisouhila218@gmail.com>
2026-04-19 11:32:19 +0200

    Build

---
+++ build/CMakeFiles/BoardMatrixDebug.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/etudiant/Documents/GitHub/Chess_Players/src/Model/Bishop.cpp" "CMakeFiles/BoardMatrixDebug.dir/src/Model/Bishop.cpp.obj" "gcc" "CMakeFiles/BoardMatrixDebug.dir/src/Model/Bishop.cpp.obj.d"
+  "C:/Users/etudiant/Documents/GitHub/Chess_Players/src/Model/Board.cpp" "CMakeFiles/BoardMatrixDebug.dir/src/Model/Board.cpp.obj" "gcc" "CMakeFiles/BoardMatrixDebug.dir/src/Model/Board.cpp.obj.d"
+  "C:/Users/etudiant/Documents/GitHub/Chess_Players/src/Model/GameState.cpp" "CMakeFiles/BoardMatrixDebug.dir/src/Model/GameState.cpp.obj" "gcc" "CMakeFiles/BoardMatrixDebug.dir/src/Model/GameState.cpp.obj.d"
+  "C:/Users/etudiant/Documents/GitHub/Chess_Players/src/Model/HexCell.cpp" "CMakeFiles/BoardMatrixDebug.dir/src/Model/HexCell.cpp.obj" "gcc" "CMakeFiles/BoardMatrixDebug.dir/src/Model/HexCell.cpp.obj.d"
+  "C:/Users/etudiant/Documents/GitHub/Chess_Players/src/Model/King.cpp" "CMakeFiles/BoardMatrixDebug.dir/src/Model/King.cpp.obj" "gcc" "CMakeFiles/BoardMatrixDebug.dir/src/Model/King.cpp.obj.d"
+  "C:/Users/etudiant/Documents/GitHub/Chess_Players/src/Model/Knight.cpp" "CMakeFiles/BoardMatrixDebug.dir/src/Model/Knight.cpp.obj" "gcc" "CMakeFiles/BoardMatrixDebug.dir/src/Model/Knight.cpp.obj.d"
+  "C:/Users/etudiant/Documents/GitHub/Chess_Players/src/Model/Pawn.cpp" "CMakeFiles/BoardMatrixDebug.dir/src/Model/Pawn.cpp.obj" "gcc" "CMakeFiles/BoardMatrixDebug.dir/src/Model/Pawn.cpp.obj.d"
+  "C:/Users/etudiant/Documents/GitHub/Chess_Players/src/Model/Piece.cpp" "CMakeFiles/BoardMatrixDebug.dir/src/Model/Piece.cpp.obj" "gcc" "CMakeFiles/BoardMatrixDebug.dir/src/Model/Piece.cpp.obj.d"
+  "C:/Users/etudiant/Documents/GitHub/Chess_Players/src/Model/PieceFactory.cpp" "CMakeFiles/BoardMatrixDebug.dir/src/Model/PieceFactory.cpp.obj" "gcc" "CMakeFiles/BoardMatrixDebug.dir/src/Model/PieceFactory.cpp.obj.d"
+  "C:/Users/etudiant/Documents/GitHub/Chess_Players/src/Model/Queen.cpp" "CMakeFiles/BoardMatrixDebug.dir/src/Model/Queen.cpp.obj" "gcc" "CMakeFiles/BoardMatrixDebug.dir/src/Model/Queen.cpp.obj.d"
+  "C:/Users/etudiant/Documents/GitHub/Chess_Players/src/Model/Rook.cpp" "CMakeFiles/BoardMatrixDebug.dir/src/Model/Rook.cpp.obj" "gcc" "CMakeFiles/BoardMatrixDebug.dir/src/Model/Rook.cpp.obj.d"
+  "C:/Users/etudiant/Documents/GitHub/Chess_Players/src/Model/boardaffichage.cpp" "CMakeFiles/BoardMatrixDebug.dir/src/Model/boardaffichage.cpp.obj" "gcc" "CMakeFiles/BoardMatrixDebug.dir/src/Model/boardaffichage.cpp.obj.d"
+  "C:/Users/etudiant/Documents/GitHub/Chess_Players/src/board_matrix_debug_main.cpp" "CMakeFiles/BoardMatrixDebug.dir/src/board_matrix_debug_main.cpp.obj" "gcc" "CMakeFiles/BoardMatrixDebug.dir/src/board_matrix_debug_main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/BoardMatrixDebug.dir/cmake_clean.cmake
@@ -0,0 +1,37 @@
+file(REMOVE_RECURSE
+  "BoardMatrixDebug.exe"
+  "BoardMatrixDebug.exe.manifest"
+  "BoardMatrixDebug.pdb"
+  "CMakeFiles/BoardMatrixDebug.dir/src/Model/Bishop.cpp.obj"
+  "CMakeFiles/BoardMatrixDebug.dir/src/Model/Bishop.cpp.obj.d"
+  "CMakeFiles/BoardMatrixDebug.dir/src/Model/Board.cpp.obj"
+  "CMakeFiles/BoardMatrixDebug.dir/src/Model/Board.cpp.obj.d"
+  "CMakeFiles/BoardMatrixDebug.dir/src/Model/GameState.cpp.obj"
+  "CMakeFiles/BoardMatrixDebug.dir/src/Model/GameState.cpp.obj.d"
+  "CMakeFiles/BoardMatrixDebug.dir/src/Model/HexCell.cpp.obj"
+  "CMakeFiles/BoardMatrixDebug.dir/src/Model/HexCell.cpp.obj.d"
+  "CMakeFiles/BoardMatrixDebug.dir/src/Model/King.cpp.obj"
+  "CMakeFiles/BoardMatrixDebug.dir/src/Model/King.cpp.obj.d"
+  "CMakeFiles/BoardMatrixDebug.dir/src/Model/Knight.cpp.obj"
+  "CMakeFiles/BoardMatrixDebug.dir/src/Model/Knight.cpp.obj.d"
+  "CMakeFiles/BoardMatrixDebug.dir/src/Model/Pawn.cpp.obj"
+  "CMakeFiles/BoardMatrixDebug.dir/src/Model/Pawn.cpp.obj.d"
+  "CMakeFiles/BoardMatrixDebug.dir/src/Model/Piece.cpp.obj"
+  "CMakeFiles/BoardMatrixDebug.dir/src/Model/Piece.cpp.obj.d"
+  "CMakeFiles/BoardMatrixDebug.dir/src/Model/PieceFactory.cpp.obj"
+  "CMakeFiles/BoardMatrixDebug.dir/src/Model/PieceFactory.cpp.obj.d"
+  "CMakeFiles/BoardMatrixDebug.dir/src/Model/Queen.cpp.obj"
+  "CMakeFiles/BoardMatrixDebug.dir/src/Model/Queen.cpp.obj.d"
+  "CMakeFiles/BoardMatrixDebug.dir/src/Model/Rook.cpp.obj"
+  "CMakeFiles/BoardMatrixDebug.dir/src/Model/Rook.cpp.obj.d"
+  "CMakeFiles/BoardMatrixDebug.dir/src/Model/boardaffichage.cpp.obj"
+  "CMakeFiles/BoardMatrixDebug.dir/src/Model/boardaffichage.cpp.obj.d"
+  "CMakeFiles/BoardMatrixDebug.dir/src/board_matrix_debug_main.cpp.obj"
+  "CMakeFiles/BoardMatrixDebug.dir/src/board_matrix_debug_main.cpp.obj.d"
+  "libBoardMatrixDebug.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BoardMatrixDebug.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
